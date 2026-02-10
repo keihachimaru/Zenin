7eb259034ca57425df9451aa3242cdc7b97661b4
keihachimaru <keihachimaru@gmail.com>
2026-02-10 16:56:54 +0100

    chore: react router setup

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,14 @@
+import '@/styles/App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home.tsx";
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App;
